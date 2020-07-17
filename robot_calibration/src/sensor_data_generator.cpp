ffbdd5d283fb8008ff08279d6ba504927538e62d
PinoOnGithub <dpino@virtanatech.com>
2020-07-17 11:18:22 -0400

    CMake changes

--- robot_calibration/src/sensor_data_generator.cpp
+++ robot_calibration/src/sensor_data_generator.cpp
@@ -2,35 +2,40 @@
 #include "sensor_msgs/JointState.h"
 #include <cmath>
 #include "yaml-cpp/yaml.h"
+#include <fstream>
 
-
-void writeAngleData(const sensor_msgs::JointState:ConstPtr& msg)
+void calculateAndWriteData(const sensor_msgs::JointState::ConstPtr& msg)
 {
-  double angles[] = msg->position;
+  double angles[2];
+  angles[0] = msg->position[0];
+  angles[1] = msg->position[1];
 
   double ee_position[] = {(1.8 * cos(angles[0])) + (1 * cos(angles[0] + angles[1])) , (1.8 * sin(angles[0])) + (1 * sin(angles[0] + angles[1]))}; //lengths currently hardcoded
   
   angles[0] += 6.44; //theta1 offset
-  angles[1] += 3.1; //theta2 offset
+  angles[1] += 3.13; //theta2 offset
 
   YAML::Emitter yaml_out_stream;
+  std::ofstream outfile;
   
-  yaml_out_stream.open("sensor_readings.yaml");
-
-  yaml_out_stream << YAML::Flow;
+  //yaml_out_stream << YAML::Flow;
   yaml_out_stream << YAML::BeginSeq << angles/*[0] << angles[1]*/ << YAML::EndSeq;
   yaml_out_stream << YAML::BeginSeq << ee_position/*[0] << ee_position[1] */<< YAML::EndSeq;
-  
-  yaml_out_stream.close();
+
+  outfile.open("sensor_data.yaml", std::fstream::app);
+  outfile << yaml_out_stream.c_str();
+  outfile.close();
+
+  return;
 }
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "simple_joint_state_listener");
 
-  ros::NodeHandle node;
+  ros::NodeHandle nh;
 
-  ros::Subscriber angles_subscriber = node.subscribe("joint_states", 100, writeAngleData);
+  ros::Subscriber angles_subscriber = nh.subscribe("joint_states", 100, calculateAndWriteData);
 
   ros::spin();
 
