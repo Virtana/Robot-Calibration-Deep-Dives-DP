5e1c7a62c7b85d6650f520d52a8845e242e07fbf
PinoOnGithub <dpino@virtanatech.com>
2020-07-24 09:57:26 -0400

    Addressing issues of last PR

--- robot_calibration/src/sensor_data_generator.cpp
+++ robot_calibration/src/sensor_data_generator.cpp
@@ -1,50 +1,85 @@
 #include "ros/ros.h"
+#include "ros/package.h"
 #include "sensor_msgs/JointState.h"
 #include <cmath>
 #include "yaml-cpp/yaml.h"
 #include <fstream>
+#include "boost/filesystem.hpp"
 
 class Listener
-{  
+{
   public:
     void calculateAndWriteData(const sensor_msgs::JointState::ConstPtr& msg);
-    std::string timestamp = std::to_string(ros::Time::now().toSec());
+    std::string filename;
+    double theta_1, theta_2, theta_1_offset, theta_2_offset;
+    double ee_position[2];
+    Listener(double t_1_offset, double t_2_offset)
+    {
+      filename = ros::package::getPath("robot_calibration");
+      if(filename != "")
+      {
+        filename += "/sensor_data";
+        boost::filesystem::create_directory(filename);
+        filename += "/sensor_data_" + std::to_string((int)ros::Time::now().toSec()) + ".yaml";
+      }
+      theta_1 = -1.0;
+      theta_2 = -1.0;
+
+      ee_position[0] = 1000000000;
+      ee_position[1] = 1000000000;
+
+      theta_1_offset = t_1_offset;
+      theta_2_offset = t_2_offset;
+    }
 };
 
 
 void Listener::calculateAndWriteData(const sensor_msgs::JointState::ConstPtr& msg)
 {
-  double angles[2];
-  angles[0] = msg->position[0];
-  angles[1] = msg->position[1];
-
   double link_1_length, link_2_length;
   link_1_length = 1.8;
   link_2_length = 1.0;
 
+  double angles[2];
+  angles[0] = msg->position[0];
+  angles[1] = msg->position[1];
+
   double ee_position[] = {(link_1_length * cos(angles[0])) + (link_2_length * cos(angles[0] + angles[1])) , (link_1_length * sin(angles[0])) + (link_2_length * sin(angles[0] + angles[1]))}; 
   
-  angles[0] += 0.044; //theta1 offset
-  angles[1] += 0.013; //theta2 offset
+  angles[0] += this->theta_1_offset; 
+  angles[1] += this->theta_2_offset;  
 
-  YAML::Emitter yaml_out_stream;
-  std::ofstream outfile;
+  if(angles[0] != this->theta_1 || angles[1] != this->theta_2)
+  {
+    this->theta_1 = angles[0];
+    this->theta_2 = angles[1];
 
-  yaml_out_stream << YAML::BeginMap;
-  yaml_out_stream << YAML::Key  << "Angles";
-  yaml_out_stream << YAML::Value;
-  yaml_out_stream << YAML::Flow;
-  yaml_out_stream << YAML::BeginSeq << angles[0] << angles[1] << YAML::EndSeq;
-  yaml_out_stream << YAML::Key  << "End Effector Position";
-  yaml_out_stream << YAML::Value;
-  yaml_out_stream << YAML::Flow;
-  yaml_out_stream << YAML::BeginSeq <<  ee_position[0] << ee_position[1] << YAML::EndSeq;
-  yaml_out_stream << YAML::EndMap;
+    YAML::Emitter yaml_out_stream;
+    std::ofstream outfile;
 
-  outfile.open("/home/jad/catkin_ws/src/Robot-Calibration-Deep-Dives-DP/robot_calibration/sensor_data/sensor_data_" + this->timestamp + ".yaml", std::fstream::app);
-  outfile << yaml_out_stream.c_str();
-  outfile.close();
+    yaml_out_stream << YAML::Literal <<"\n";
+    yaml_out_stream << YAML::BeginMap;
+    yaml_out_stream << YAML::Key  << "Angles";
+    yaml_out_stream << YAML::Value;
+    yaml_out_stream << YAML::Flow;
+    yaml_out_stream << YAML::BeginSeq << angles[0] << angles[1] << YAML::EndSeq;
+    yaml_out_stream << YAML::Key  << "End Effector Position";
+    yaml_out_stream << YAML::Value;
+    yaml_out_stream << YAML::Flow;
+    yaml_out_stream << YAML::BeginSeq <<  ee_position[0] << ee_position[1] << YAML::EndSeq;
+    yaml_out_stream << YAML::EndMap;
 
+    if(filename == "")
+    {
+      ROS_WARN("%s","Could not locate package in attempting to save sensor data.");
+    }
+    else
+    {
+      outfile.open(filename, std::fstream::app);
+      outfile << yaml_out_stream.c_str();
+      outfile.close();
+    }   
+  }
   return;
 }
 
@@ -54,9 +89,14 @@ int main(int argc, char** argv)
 
   ros::NodeHandle nh;
 
-  Listener listener;
+  double theta_1_offset, theta_2_offset;
 
-  ros::Subscriber angles_subscriber = nh.subscribe("joint_states", 100, &Listener::calculateAndWriteData, &listener);
+  nh.getParam("/theta_1_offset", theta_1_offset);
+  nh.getParam("/theta_2_offset", theta_2_offset);
+
+  Listener listener(theta_1_offset, theta_2_offset);
+
+  ros::Subscriber angles_subscriber = nh.subscribe("joint_states", 10, &Listener::calculateAndWriteData, &listener);
 
   ros::spin();
 
